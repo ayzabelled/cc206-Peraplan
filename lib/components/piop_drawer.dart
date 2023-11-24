f5011a850ae9afbb00c6c2e733b3fce9a5418639
Allana Yzabelle M. Diaz <allanayzabelle.diaz@wvsu.edu.ph>
2023-11-24 16:19:55 +0800

    added drawer

---
+++ lib/components/piop_drawer.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:peraplan/features/Transactions_Screen.dart';
+
+class PIOPDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      // Drawer content goes here
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(colors: [hlblue, text]),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                // Icon in the drawer header
+                const Icon(
+                  Icons.menu_book_outlined,
+                  color: Colors.white,
+                  size: 64.0,
+                ),
+                SizedBox(width: small),
+                Text('Quick Menu', style: drawHead),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.add,
+              color: text,
+            ),
+            title: Text(
+              'New Transaction',
+              style: drawTxt,
+            ),
+            //  onTap: () {
+            // Handle item 1 tap
+            //  Navigator.push(
+            //      context,
+            //  MaterialPageRoute(builder: (context) => const NewTransaction()),
+            //      );
+            //     },
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.receipt_long,
+              color: text,
+            ),
+            title: Text(
+              'Transaction History',
+              style: drawTxt,
+            ),
+            onTap: () {
+              // Handle item 2 tap
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const TransactionsScreen()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
